5e2c3d86c79f0bd9f3cfce1abebd01b632341568
Mattias Andrée <maandree@operamail.com>
2012-07-27 05:04:27 +0200

    Second commit

---
+++ src/se/kth/maandree/tagdir/Program.java
@@ -0,0 +1,48 @@
+/**
+ *  tagdir — Tag files to add and remove the from directories
+ *  Copyright © 2012  Mattias Andrée
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.kth.maandree.tagdir;
+
+
+/**
+ * The is the main class of the program
+ * 
+ * @author  Mattias Andrée, <a href="mailto:maandree@kth.se">maandree@kth.se</a>
+ */
+public class Program
+{
+    /**
+     * Non-constructor
+     */
+    private Program()
+    {
+        assert false : "You may not create instances of this class [Program].";
+    }
+    
+    
+    
+    /**
+     * This is the main entry point of the program
+     * 
+     * @param  args  Startup arguments, unused
+     */
+    public static void main(final String... args)
+    {
+    }
+    
+}
+
