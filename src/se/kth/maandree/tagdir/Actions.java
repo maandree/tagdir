4dbad34102113450f3ff79eab44ca59103ef423d
Mattias Andrée <maandree@operamail.com>
2012-10-16 15:04:16 +0200

    implementing some actions

--- src/se/kth/maandree/tagdir/Actions.java
+++ src/se/kth/maandree/tagdir/Actions.java
@@ -18,6 +18,10 @@
 package se.kth.maandree.tagdir;
 
 import java.sql.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOptions;
 
 
 /**
@@ -38,6 +42,16 @@ public class Actions
     
     
     /**
+     * Gets the current working directory's file path
+     * 
+     * @return  The current working directory's file path
+     */
+    private static String getDirPath()
+    {
+	return "";
+    }
+    
+    /**
      * Gets the current working directory's ID
      * 
      * @return  The current working directory's ID
@@ -45,6 +59,15 @@ public class Actions
     private static int getDirID()
     {
 	return 0;
+    }
+    
+    /**
+     * Sets the current working directory's ID
+     * 
+     * @param  id  The current working directory's ID
+     */
+    private static void setDirID(final int id)
+    {
     }
     
     /**
@@ -57,6 +80,16 @@ public class Actions
 	return "";
     }
     
+    /**
+     * Creates a file hard link
+     * 
+     * @param  target  The target file
+     * @param  source  The source file
+     */
+    private static void hardlink(final String target, final String source)
+    {
+    }
+    
     
     
     /**
@@ -66,6 +99,9 @@ public class Actions
      */
     public static void install() throws SQLException
     {
+	System.out.println("Creating database...");
+	Functions.install(fetchPassword());
+	System.out.println("Finished.");
     }
     
     
@@ -76,6 +112,9 @@ public class Actions
      */
     public static void init() throws SQLException
     {
+	System.out.println("Initialing catalogue...");
+	setDirID(Functions.init(fetchPassword(), getDirPath()));
+	System.out.println("Finished.");
     }
     
     
@@ -86,6 +125,26 @@ public class Actions
      */
     public static void ls() throws SQLException
     {
+	final String[][] pairs = Functions.ls(fetchPassword(), getDirPath());
+	String last = null;
+	for (final String[] pair : pairs)
+	{
+	    final String file = pair[0];
+	    final String tag = pair[1];
+	    
+	    if ((last == null) || (last.equals(file) == false))
+	    {
+		if (last != null)
+		    System.out.println("]");
+		System.out.print("'" + file.replace("'", "'\\''") + "'");
+		System.out.print(" [ " + tag.replace("'", "'\\''") + " ");
+		last = file;
+	    }
+	    else
+		System.out.print(tag.replace("'", "'\\''") + " ");
+	}
+	if (last != null)
+	    System.out.println("]");
     }
     
     
@@ -96,6 +155,9 @@ public class Actions
      */
     public static void all() throws SQLException
     {
+	final String[] files = Functions.ls(fetchPassword(), getDirPath());
+	for (final String file : files)
+	    System.out.println("'" + file.replace("'", "'\\''") + "'");
     }
     
     
@@ -106,6 +168,9 @@ public class Actions
      */
     public static void tags() throws SQLException
     {
+	final String[] tags = Functions.tags(fetchPassword(), getDirPath());
+	for (final String tag : tags)
+	    System.out.println("'" + tag.replace("'", "'\\''") + "'");
     }
     
     
@@ -118,6 +183,26 @@ public class Actions
      */
     public static void tags(final String[] files) throws SQLException
     {
+	final String[][] pairs = Functions.tags(fetchPassword(), getDirPath(), files);
+	String last = null;
+	for (final String[] pair : pairs)
+	{
+	    final String file = pair[0];
+	    final String tag = pair[1];
+	    
+	    if ((last == null) || (last.equals(file) == false))
+	    {
+		if (last != null)
+		    System.out.println("]");
+		System.out.print("'" + file.replace("'", "'\\''") + "'");
+		System.out.print(" [ " + tag.replace("'", "'\\''") + " ");
+		last = file;
+	    }
+	    else
+		System.out.print(tag.replace("'", "'\\''") + " ");
+	}
+	if (last != null)
+	    System.out.println("]");
     }
     
     
@@ -130,6 +215,24 @@ public class Actions
      */
     public static void rm(final String[] files) throws SQLException
     {
+	final String[] files = Functions.rm(fetchPassword(), getDirPath(), files);
+	for (final String file : files)
+	{
+	    System.out.println("Removing '" + tag.replace("'", "'\\''") + "'");
+	    try
+	    {
+		final boolean c = (new File("./" + file)).exists();
+		final boolean a = (new File("./.tagdir/" + file)).exists();
+		if (c)  (new File("./" + file)).delete();
+		if (a)  (new File("./.tagdir/" + file)).delete();
+		if (!c && !a)
+		    System.out.println("No such file");
+	    }
+	    catch (final Throwable err)
+	    {
+		System.out.println(err.toString());
+	    }
+	}
     }
     
     
@@ -142,6 +245,28 @@ public class Actions
      */
     public static void untag(final String[] files) throws SQLException
     {
+	Functions.untag(fetchPassword(), getDirPath(), files);
+	for (final String file : files)
+	    try
+	    {
+		final boolean c = (new File("./" + file)).exists();
+		final boolean a = (new File("./.tagdir/" + file)).exists();
+		
+		if (c && a)
+		    (new File("./.tagdir/" + file)).delete();
+		else if (c)
+		{
+		    // Do nothing
+		}
+		else if (a)
+		    Files.move(Paths.get("./.tagdir/" + file), Paths.get("./" + file), StandardCopyOptions.ATOMIC_MOVE);
+		else
+		    System.out.println("No such file");
+	    }
+	    catch (final Throwable err)
+	    {
+		System.out.println(err.toString());
+	    }
     }
     
     
@@ -155,6 +280,23 @@ public class Actions
      */
     public static void mv(final String src, final String dest) throws SQLException
     {
+	final boolean cs = (new File("./" + src)).exists();
+	final boolean as = (new File("./.tagdir/" + src)).exists();
+	final boolean cd = (new File("./" + dest)).exists();
+	final boolean ad = (new File("./.tagdir/" + dest)).exists();
+	
+	if (cd | ad)
+	    System.out.println("Destination file already exists");
+	else if (!cs && !as)
+	    System.out.println("No such file");
+	else
+	{
+	    Functions.mv(fetchPassword(), getDirPath(), src, dest);
+	    if (cs)
+		Files.move(Paths.get("./" + src), Paths.get("./" + dest), StandardCopyOptions.ATOMIC_MOVE);
+	    if (as)
+		Files.move(Paths.get("./.tagdir/" + src), Paths.get("./.tagdir/" + dest), StandardCopyOptions.ATOMIC_MOVE);
+	}
     }
     
     
@@ -169,6 +311,20 @@ public class Actions
      */
     public static void show(final String[] as, final String[] ns, final String[] ms) throws SQLException
     {
+	final String[] files = Functions.tagFilter(fetchPassword(), getDirPath(), as, ns, ms);
+	for (final String file : (new File("./")).list())
+	    if ((new File("./" + file)).isDirectory() == false)
+		if ((new File("./.tagdir/" + file)).exists())
+		    (new File("./" + file)).delete();
+	for (final String file : files)
+	    if ((new File("./.tagdir/" + file)).exists())
+	    {
+		System.out.println("File '" + file.replace("'", "'\\''") + "' does not exist is archive");
+	        return;
+	    }
+	for (final String file : files)
+	    if ((new File("./" + file)).exists() == false)
+	        hardlink("./.tagdir/" + file, "./" + file);
     }
     
     
@@ -183,6 +339,16 @@ public class Actions
      */
     public static void also(final String[] as, final String[] ns, final String[] ms) throws SQLException
     {
+	final String[] files = Functions.tagFilter(fetchPassword(), getDirPath(), as, ns, ms);
+	for (final String file : files)
+	    if ((new File("./.tagdir/" + file)).exists())
+	    {
+		System.out.println("File '" + file.replace("'", "'\\''") + "' does not exist is archive");
+	        return;
+	    }
+	for (final String file : files)
+	    if ((new File("./" + file)).exists() == false)
+	        hardlink("./.tagdir/" + file, "./" + file);
     }
     
     
@@ -197,26 +363,16 @@ public class Actions
      */
     public static void hide(final String[] as, final String[] ns, final String[] ms) throws SQLException
     {
-    }
-    
-    
-    /**
-     * Export all data for the directory
-     * 
-     * @throws  SQLException  On database error
-     */
-    public static void export() throws SQLException
-    {
-    }
-    
-    
-    /**
-     * Import exported directory data
-     * 
-     * @throws  SQLException  On database error
-     */
-    public static void importSave() throws SQLException
-    {
+	final String[] files = Functions.tagFilter(fetchPassword(), getDirPath(), as, ns, ms);
+	for (final String file : files)
+	    if ((new File("./.tagdir/" + file)).exists())
+	    {
+		System.out.println("File '" + file.replace("'", "'\\''") + "' does not exist is archive");
+	        return;
+	    }
+	for (final String file : files)
+	    if ((new File("./" + file)).exists())
+		(new File("./" + file)).delete();
     }
     
     
@@ -229,44 +385,75 @@ public class Actions
      */
     public static void tag(final String[] params) throws SQLException
     {
+	int index = -1;
+	for (int i = 0, n = params.length(); i < n; i++)
+	    if ((params[i] == '=') || (params[i] == '+') || (params[i] == '-'))
+	    {
+		index = i;
+		break;
+	    }
+	if ((index < 1) || (index == params.length - 1))
+	{
+	    System.out.println("Syntax error");
+	    return;
+	}
+        final String[] files = new String[index];
+        final String[] tags = new String[params.length - 1 - index];
+	System.arraycopy(params, 0, files, 0, files.length);
+	System.arraycopy(params, index + 1, tags, 0, tags.length);
+	boolean stop = false;
+	for (final String file : files)
+	{
+	    final boolean c = (new File("./" + file)).exists();
+	    final boolean a = (new File("./.tagdir/" + file)).exists();
+	    if (!c && !a)
+	    {
+		System.out.println("File '" + file.replace("'", "'\\''") + "' does not exist");
+		break;
+	    }
+	}
+	if (stop)
+	{
+	    System.out.println("Aborting");
+	    return;
+	}
+	for (final String file : files)
+	{
+	    final boolean a = (new File("./.tagdir/" + file)).exists();
+	    if (!a)
+		hardlink("./" + file, "./.tagdir/" + file);
+	}
+	if (params[i] == '=')
+	    Functions.tagSet(fetchPassword(), getDirPath(), files, tags);
+	else if (params[i] == '+')
+	    Functions.tagAdd(fetchPassword(), getDirPath(), files, tags);
+	else if (params[i] == '-')
+	    Functions.tagRemove(fetchPassword(), getDirPath(), files, tags);
     }
     
     
     /**
-     * Set tags for file
-     * 
-     * @param  files  The files of interest
-     * @param  tag    The tags
+     * Export all data for the directory
      * 
      * @throws  SQLException  On database error
      */
-    private static void tagSet(final String[] files, final String[] tags) throws SQLException
+    public static void export() throws SQLException
     {
+	String[][] data = Functions.export(fetchPassword(), getDirPath());
+	for (final String[] tuple : data)
+	    System.out.println("( file: '"  + tuple[0].replace("'", "'\\''")
+			       + "' dir: '" + tuple[1].replace("'", "'\\''")
+			       + "' tag: '" + tuple[2].replace("'", "'\\''")
+			       + "' )");
     }
     
     
     /**
-     * Add tag to file
-     * 
-     * @param  files  The files of interest
-     * @param  tag    The tags
+     * Import exported directory data
      * 
      * @throws  SQLException  On database error
      */
-    private static void tagAdd(final String[] files, final String[] tags) throws SQLException
-    {
-    }
-    
-    
-    /**
-     * Remove tag from file
-     * 
-     * @param  files  The files of interest
-     * @param  tag    The tags
-     * 
-     * @throws  SQLException  On database error
-     */
-    private static void tagRemove(final String[] files, final String[] tags) throws SQLException
+    public static void importSave() throws SQLException
     {
     }
     
